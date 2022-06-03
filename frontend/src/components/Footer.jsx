89784d8bd62ce66e45a364ad7a54cfc60062e636
Aymen <aymensbc@gmail.com>
2022-06-03 14:46:26 +0500

    Create footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,135 @@
+import {
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Pinterest,
+  Room,
+  Twitter,
+} from "@mui/icons-material";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const Left = styled.div`
+  flex-direction: column;
+  display: flex;
+  flex: 1;
+  padding: 20px;
+`;
+const Logo = styled.h1``;
+const Desc = styled.p`
+  margin: 20px 0;
+`;
+const SocialContainer = styled.div`
+  display: flex;
+`;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 10px;
+`;
+const Center = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>SURVEY CORPS.</Logo>
+        <Desc>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo soluta
+          omnis, dolore adipisci asperiores neque veritatis illo commodi,
+          doloremque voluptas laborum esse quos vitae, sint hic? Dolorum
+          incidunt sit inventore.
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Men's Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room />
+          Outside the Walls
+        </ContactItem>
+        <ContactItem>
+          <Phone />
+          +12 345 675
+        </ContactItem>
+        <ContactItem>
+          <MailOutline />
+          conatact@surveyCorps
+        </ContactItem>
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Announcement from "../components/Announcement";
 import Categories from "../components/Categories";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Newsletter from "../components/Newsletter";
 import Product from "../components/Product";
@@ -14,6 +15,7 @@ const Home = () => {
       <Categories />
       <Product />
       <Newsletter />
+      <Footer />
     </div>
   );
 };
