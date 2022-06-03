93bceef370c9307d94573a50252b7104eb4b9636
Aymen <aymensbc@gmail.com>
2022-06-03 12:00:21 +0500

    add hover affect on icons and opacity changes

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -5,25 +5,8 @@ import {
   ShoppingCartOutlined,
 } from "@mui/icons-material";
 
-const Container = styled.div`
-  flex: 1;
-  margin: 5px;
-  min-width: 280px;
-  height: 350px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: #f5fbfd;
-  position: relative;
-`;
-const Circle = styled.div`
-  width: 200px;
-  height: 200px;
-  border-radius: 50%;
-  background-color: white;
-  position: absolute;
-`;
 const Info = styled.div`
+  opacity: 0;
   width: 100%;
   height: 100%;
   top: 0;
@@ -34,7 +17,32 @@ const Info = styled.div`
   justify-content: center;
   align-items: center;
   background-color: rgba(0, 0, 0, 0.2);
+  transition: all 0.5s ease;
 `;
+
+const Container = styled.div`
+  flex: 1;
+  margin: 5px;
+  min-width: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5fbfd;
+  position: relative;
+
+  &:hover ${Info} {
+    opacity: 1;
+  }
+`;
+const Circle = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
+`;
+
 const Image = styled.img`
   height: 75%;
   z-index: 2;
@@ -49,6 +57,7 @@ const Icon = styled.div`
   justify-content: center;
   margin: 10px;
   transition: all 0.5s ease;
+  cursor: pointer;
 
   &:hover {
     background-color: #e9f5f5;
